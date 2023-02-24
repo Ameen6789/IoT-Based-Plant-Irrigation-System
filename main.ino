3feec6775a84719be7dc843ef8fc436c461d1a0c
Azarath Ameen C A <70131572+Ameen6789@users.noreply.github.com>
2023-02-24 11:19:36 +0530

    Wifi Connect

--- main.ino
+++ main.ino
@@ -1,1 +1,31 @@
+// Include Library
+#include <ESP8266WiFi.h>
 
+
+// Constants
+#define SSID	""
+#define PWD	""
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin(SSID, PWD);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+}
