bc34934d3c604a5973704a0ab83af1f77e98a221
SharathKumar2001 <107829504+SharathKumar2001@users.noreply.github.com>
2023-02-24 14:19:54 +0530

    Added manual control of solenoid valve
    - Added manual control of solenoid valve
    -  Started implementing blyink cloud integration

--- main.ino
+++ main.ino
@@ -1,15 +1,17 @@
 // Include Library
 #include <ESP8266WiFi.h>
 #include <TimeLib.h>
-
+#include <BlynkSimpleEsp8266.h>
 
 // Constants
-#define SSID                ""
-#define PWD                 ""
+#define SSID                ""      // WiFi ssid
+#define PWD                 ""      // WiFi password
+#define auth                ""      // Blynk authtoken
 #define solenoidPin         D1
 #define moisturePin         A0
 #define moistureThreshold   60;
 
+WiFiServer server(80);
 
 void setup()
 {
@@ -26,11 +28,44 @@ void setup()
     Serial.print(".");
   }
 
+  Blynk.begin(auth, SSID, PWD, "blynk.cloud", 80);
+
+
   server.begin();
   Serial.println("Server started");
 }
 
 void loop() {
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n') {
+          currentLine = "";
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+          client.println("<html>");
+          client.println("<head><title>ESP8266 Solenoid Valve Control</title></head>");
+          client.println("<body>");
+          client.println("<h1>ESP8266 Solenoid Valve Control</h1>");
+          client.println("<p>Solenoid valve is currently " + String(digitalRead(solenoidPin) == HIGH ? "open" : "closed") + "</p>");
+          client.println("</body>");
+          client.println("</html>");
+          break;
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+
   if (hour() == 7 && minute() == 0 && second() == 0) {
     digitalWrite(solenoidPin, HIGH);
     Serial.println("Solenoid valve opened");
